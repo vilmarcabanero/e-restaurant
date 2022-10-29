3c519e35106b951ab52278b0ebd99ab2492c78cc
Vilmar Cabañero <vilmarcabanero@gmail.com>
2022-10-29 18:31:12 +0800

    Done with chapter 3 retrofit.

--- app/src/main/java/net/entropiya/erestaurant/RestaurantsViewModel.kt
+++ app/src/main/java/net/entropiya/erestaurant/RestaurantsViewModel.kt
@@ -3,9 +3,28 @@ package net.entropiya.erestaurant
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewModel() {
-    val state = mutableStateOf(dummyRestaurants.restoreSelections())
+    private var restInterface: RestaurantsApiService
+    val state = mutableStateOf(emptyList<Restaurant>())
+    private lateinit var restaurantsCall: Call<List<Restaurant>>
+    init {
+        val retrofit: Retrofit = Retrofit.Builder().addConverterFactory(
+            GsonConverterFactory.create()
+        ).baseUrl(
+            "https://erestaurant-bfd3d-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        ).build()
+        restInterface = retrofit.create(
+            RestaurantsApiService::class.java
+        )
+        getRestaurants()
+    }
+
     fun toggleFavorite(id: Int) {
         val restaurants = state.value.toMutableList()
         val itemIndex = restaurants.indexOfFirst { it.id == id }
@@ -35,5 +54,29 @@ class RestaurantsViewModel(private val stateHandle: SavedStateHandle) : ViewMode
             return restaurantsMap.values.toList()
         }
         return this
+    }
+
+    private fun getRestaurants() {
+        restaurantsCall = restInterface.getRestaurants()
+        restaurantsCall.enqueue(object : Callback<List<Restaurant>> {
+            override fun onResponse(
+                call: Call<List<Restaurant>>, response: Response<List<Restaurant>>
+            ) {
+                response.body()?.let { restaurants ->
+                    state.value = restaurants.restoreSelections()
+                }
+            }
+
+            override fun onFailure(
+                call: Call<List<Restaurant>>, t: Throwable
+            ) {
+                t.printStackTrace()
+            }
+        })
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        restaurantsCall.cancel()
     }
 }
